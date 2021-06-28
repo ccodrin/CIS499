9552017204ea77b854a50e7822e976e583dcbd2a
Codrin <44957946+ccodrin@users.noreply.github.com>
2021-06-27 20:34:22 -0400

    Update send.py

--- Hardware Code /Raspberry Pi Code/send.py
+++ Hardware Code /Raspberry Pi Code/send.py
@@ -1,7 +1,7 @@
 import socket
 
-myfile = open("/home/pi/Desktop/SendData.json", "rt") # open test.txt for reading text this file is located where the py program is located
-contents = myfile.read()         # read the entire file to string
+myfile = open("/home/pi/Desktop/SendData.json", "rt") # open SendData.json 
+contents = myfile.read()     # read the entire file to string
 myfile.close()
 
 # Create a socket object 
@@ -10,11 +10,16 @@ s = socket.socket()
 # Define the port on which you want to connect 
 port = 1619
 
-# connect to the server on local computer 
-s.connect(('sql.SECRETPORT.com', port)) 
+try:
+    # connect to the server 
+    s.connect(('sql.YOUR PERSONAL SERVER.com', port))  #Change to your own server
 
-# receive data from the server 
-print (s.recv(4069).decode())
-s.send(contents.encode() )
-# close the connection 
-s.close()
+    # receive data from the server 
+    print (s.recv(4069).decode()) 
+    #send our Info to the Database
+    s.send(contents.encode() )
+    # close the connection 
+    s.close()
+except:
+    # If the communication to and from Database has failed, let us know
+    print("The data failed to be sent.") 
