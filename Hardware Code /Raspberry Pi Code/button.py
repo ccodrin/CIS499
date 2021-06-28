1370466c1aa060ee92ea9efbab2fd31c6cf8618f
Codrin <44957946+ccodrin@users.noreply.github.com>
2021-06-27 21:06:23 -0400

    Update button.py

--- Hardware Code /Raspberry Pi Code/button.py
+++ Hardware Code /Raspberry Pi Code/button.py
@@ -8,29 +8,29 @@ GPIO.setup(17, GPIO.OUT)  #LED Green to GPIO17
 GPIO.setup(27, GPIO.OUT)#LED Red to GPIO27
 GPIO.output(27, False)
 GPIO.output(17, False)
-
-num=0
+#num is a counter
+num=0 
 
 
 try:
     while True:
-         button_state = GPIO.input(22)
-         if button_state == True:
-             if num%2==0:
-                f = open("but.txt","w+")
-                f.write("0")
+         button_state = GPIO.input(22) #Check if the button is pressed
+         if button_state == True: 
+             if num%2==0: #check if it is even number
+                f = open("but.txt","w+") #open the text file called but.txt 
+                f.write("0") # write a 0 in it for status in 
                 f.close()
-                GPIO.output(17, True)
-                GPIO.output(27, False)
+                GPIO.output(17, True) #turn on green light
+                GPIO.output(27, False) #turn off red light
              else:
-                f = open("but.txt","w+")
-                f.write("1")
+                f = open("but.txt","w+")#open the text file called but.txt 
+                f.write("1") # write a 0 in it for status in 
                 f.close()
-                GPIO.output(27, True)
-                GPIO.output(17, False)
+                GPIO.output(27, True) #turn on red light
+                GPIO.output(17, False) #turn off green light
                 
-             num+=1;   
-             print('Button Pressed...',num)
+             num+=1;   #increment counter
+             print('Button Pressed...',num) #for troubleshooting | checking the counter
              time.sleep(0.3)
          
 except:
